e161ec219cadc3740ee3f66398bf0e0ee771a293
xzqiaochu <xzzhangqiaochu@163.com>
2025-11-13 04:23:37 +0800

    支持接入摄像头视频流

--- video.py
+++ video.py
@@ -6,6 +6,8 @@ from collections import deque
 import time
 import logging
 
+# ffmpeg列出摄像头
+# ffmpeg -list_devices true -f dshow -i dummy
 
 class Video(threading.Thread):
     def __init__(self, level=logging.WARNING):
@@ -42,7 +44,12 @@ class Video(threading.Thread):
             if self._container is None:
                 self.logger.info(f"尝试连接视频源: {self._source}")
                 try:
-                    self._container = av.open(self._source, format='dshow', options={"timeout": "3000000"})  # 超时3秒（单位：微秒）
+                    if self._source.startswith("video="):
+                        self._container = av.open(self._source, format='dshow') 
+                    elif self._source.startswith("/dev"):
+                        self._container = av.open(self._source, format='v4l2') 
+                    else:
+                        self._container = av.open(self._source, options={"timeout": "3000000"})  # 超时3秒（单位：微秒）
                     self.logger.info(f"视频源连接成功")
                 except Exception as e:
                     self.logger.info(f"连接视频源报错: {e}")
@@ -56,7 +63,11 @@ class Video(threading.Thread):
             av_frame = next(self._container.decode(video=0))
             # PyAV 返回的帧是 AVFrame 对象，需要转换为 numpy 数组给 OpenCV 使用（BGR格式）
             frame = av_frame.to_ndarray(format="bgr24")
-            self.frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            height, width = frame.shape[:2]
+            if height > width:
+                self.frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            else:
+                self.frame = frame
             self._update_fps()
         except Exception as e:
             self.logger.error(f"读取视频流报错: {e}")
@@ -89,7 +100,8 @@ if __name__ == "__main__":
 
     video = Video(logging.INFO)
     video.start()
-    source = "rtsp://192.168.1.1:7070/webcam"  # 请替换为实际的视频源
+    source = "rtsp://192.168.1.1:7070/webcam"
+    # source = "video=Integrated Camera"
     video.set_source(source)
 
     while True:
